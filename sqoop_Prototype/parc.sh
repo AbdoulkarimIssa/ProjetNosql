14b5abf2a9e4248d2e0a07200bd40de15486a8c9
AbdoulkarimIssa <issabdoulkarim@gmail.com>
2022-06-03 11:22:37 +0200

    modification des commandes hdfs

--- sqoop_Prototype/parc.sh
+++ sqoop_Prototype/parc.sh
@@ -2,10 +2,10 @@
 
 ## Ce script remonte toute les données de la base parc vers hdfs
 
-hdfs dfs -rmdir /user/maria_dev/parc/compteurs #suppression de l'ancien fichier
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table compteurs -m 1 --target-dir /user/maria_dev/parc/compteurs
+hdfs dfs -rm -r /user/maria_dev/parc/compteurs #suppression de l'ancien fichier
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table compteurs -m 1 --target-dir /user/maria_dev/parc/compteurs
 
-hdfs dfs -rmdir /user/maria_dev/parc/concentrateurs
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table concentrateurs -m 1 --target-dir /user/maria_dev/parc/concentrateurs
+hdfs dfs -rm -r /user/maria_dev/parc/concentrateurs
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table concentrateurs -m 1 --target-dir /user/maria_dev/parc/concentrateurs
 
 
--- sqoop_Prototype/reseaux.sh
+++ sqoop_Prototype/reseaux.sh
@@ -2,20 +2,20 @@
 
 ## Ce script remonte les tables de la base de données reseaux vers HDFS
 
-hdfs dfs -rmdir /user/maria_dev/reseaux/pdc
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table pdc -m 1 --target-dir /user/maria_dev/reseaux/pdc
+hdfs dfs -rm -r /user/maria_dev/reseaux/pdc
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table pdc -m 1 --target-dir /user/maria_dev/reseaux/pdc
 
-hdfs dfs -rmdir /user/maria_dev/reseaux/pdk
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table pdk -m 1 --target-dir /user/maria_dev/reseaux/pdk
+hdfs dfs -rm -r /user/maria_dev/reseaux/pdk
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table pdk -m 1 --target-dir /user/maria_dev/reseaux/pdk
 
-hdfs dfs -rmdir /user/maria_dev/reseaux/communes
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table communes -m 1 --target-dir /user/maria_dev/reseaux/communes
+hdfs dfs -rm -r /user/maria_dev/reseaux/communes
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table communes -m 1 --target-dir /user/maria_dev/reseaux/communes
 
-hdfs dfs -rmdir /user/maria_dev/reseaux/regions
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table region -m 1 --target-dir /user/maria_dev/reseaux/regions
+hdfs dfs -rm -r /user/maria_dev/reseaux/regions
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table region -m 1 --target-dir /user/maria_dev/reseaux/regions
 
-hdfs dfs -rmdir /user/maria_dev/reseaux/departement
-sqoop import --connect jdbc:mysql://192.168.1.17:3306/parc --username root --password root  --table departements -m 1 --target-dir /user/maria_dev/reseaux/departement
+hdfs dfs -rm -r /user/maria_dev/reseaux/departement
+sqoop import --connect jdbc:mysql://161.3.45.94:3306/parc --username root --password root  --table departements -m 1 --target-dir /user/maria_dev/reseaux/departement
 
 
 
